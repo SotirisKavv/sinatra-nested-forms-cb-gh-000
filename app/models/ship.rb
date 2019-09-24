5a534ef72869f9df085a762cd7247cb2cb65e4d7
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 20:52:58 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,27 @@
 class Ship
+  class Pirate
+
+     attr_reader :name, :type, :booty
+
+     @@all = []
+
+     def initialize(name, type, booty)
+       @name = name
+       @type = type
+       @booty = booty
+       self.save
+     end
+
+     def self.all
+       @@all
+     end
+
+     def save
+       @@all << self
+     end
+
+     def self.clear
+       self.all.clear       
+     end
+  end
 end

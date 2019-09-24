d35938cc4806879722653a6327831b019302483e
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 21:30:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ module FormsLab
     end
 
     get '/new' do
-      erb :pirates/new
+      erb :"pirates/new"
     end
 
     post '/pirates' do
-      erb :show
+      erb :"pirates/show"
     end
 
   end

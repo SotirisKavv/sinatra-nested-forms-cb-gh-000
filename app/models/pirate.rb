d7d2018ae34e19fcc5fb7c68686a622029f6009e
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-24 20:51:10 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,9 +2,20 @@ class Pirate
 
    attr_reader :name, :weight, :height
 
+   @@all = []
+   
    def initialize(name, weight, height)
      @name = name
      @weight = weight
      @height = height
+     self.save
+   end
+
+   def self.all
+     @@all
+   end
+
+   def save
+     @@all << self
    end
 end
